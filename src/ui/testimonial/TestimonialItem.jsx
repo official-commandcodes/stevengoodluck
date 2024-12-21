bf202174c902071a3efb50958b9dd1880f941a7c
commandcodes <musaabdulkabeer19@gmail.com>
2024-12-21 10:06:43 +0100

    fix: fixing

--- src/ui/testimonial/TestimonialItem.jsx
+++ src/ui/testimonial/TestimonialItem.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 function TestimonialItem({ children, image, testimonyBy }) {
   return (
     <div className="px-2 md:p-4 h-full flex items-stretch">
-      <div className="p-6 bg-customGray-100 rounded-[1rem]">
-        <img src={image} alt={testimonyBy} />
-        <div className="py-6 text-[1.4rem] font-light">{children}</div>
+      <div className="p-6 bg-customGray-100 rounded-[0.8rem]">
+        <img src={image} alt={testimonyBy} className="w-[3rem] h-[3rem]" />
+        <div className="py-6 text-[1rem] leading-[24px] md:text-[1.4rem] font-light">{children}</div>
         <span className="text-[1.2rem]">- {testimonyBy}</span>
       </div>
     </div>
